531cc6374a9c7620f79761b648ba496dc34d9155
edukshare-max <edukshare-max@users.noreply.github.com>
2025-10-08 10:14:43 +0000

    deploy: 7e6b53de6a0000dc75518e83f975c32d30b4d0d7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1531246702"
+    serviceWorkerVersion: "329268508"
   }
 });
