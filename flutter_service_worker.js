639d0c77d786a4b928967e1d2c2feb891eb0f715
edukshare-max <edukshare-max@users.noreply.github.com>
2025-10-08 08:03:27 +0000

    deploy: 72e35078a5c566d29ec6e59dabb5a166db6522c0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "carousel-modal-styles.css": "8f9fedd5ec8fe7953cf7cb22e7115927",
 "fix_login.js": "6c88c66e979bb5728fd476746799c14c",
 "api_config.js": "f29432ce71fce2f7107ed5b9b9eb3f69",
-"flutter_bootstrap.js": "8e990755c8a5cd5a35068d8491151140",
+"flutter_bootstrap.js": "4173abdefa7e40ce7d0398cf7ff98325",
 "version.json": "319e359c9bc4b44401abedbc3d5cea99",
 "index-flutter-original.html": "3af4b98ce3c13a646890b96317d590fc",
 "main.dart.js": "90e913b2853b4408f2e2d340bcf8f1b1"};
